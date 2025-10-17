641ae4aa484e05d18c817c03914656a66d29ff42
민수아 <Minsua0503@gmail.com>
2025-10-17 12:21:47 +0900

    init

---
+++ src/main/java/com/example/urikkiriserver/UrikkiriServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.urikkiriserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrikkiriServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrikkiriServerApplication.class, args);
+	}
+
+}
