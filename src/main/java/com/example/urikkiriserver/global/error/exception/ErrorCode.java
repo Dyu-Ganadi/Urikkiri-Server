214b7277c07b9c8a7e398c3e6c9cb1893808953c
민수아 <Minsua0503@gmail.com>
2025-12-12 11:15:01 +0900

    feat: ErrorCode

---
+++ src/main/java/com/example/urikkiriserver/global/error/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.example.urikkiriserver.global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+
+    private final HttpStatus status;
+    private final String message;
+}
