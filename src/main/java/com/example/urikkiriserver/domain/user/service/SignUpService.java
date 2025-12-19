cdf2ede62b27a8a5a470434e1fe9c44c3a9c3a2e
민수아 <Minsua0503@gmail.com>
2025-12-19 12:17:48 +0900

    feat: SignUpService

---
+++ src/main/java/com/example/urikkiriserver/domain/user/service/SignUpService.java
@@ -0,0 +1,42 @@
+package com.example.urikkiriserver.domain.user.service;
+
+import com.example.urikkiriserver.domain.user.domain.User;
+import com.example.urikkiriserver.domain.user.domain.repository.UserRepository;
+import com.example.urikkiriserver.domain.user.exception.UserExists;
+import com.example.urikkiriserver.domain.user.presentation.dto.request.SignUpRequest;
+import com.example.urikkiriserver.domain.user.presentation.dto.response.TokenResponse;
+import com.example.urikkiriserver.global.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SignUpService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+    @Transactional
+    public TokenResponse execute(SignUpRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw UserExists.EXCEPTION;
+        }
+
+        if (userRepository.existsByNickname(request.nickname())) {
+            throw UserExists.EXCEPTION;
+        }
+
+        userRepository.save(
+            User.builder()
+                .email(request.email())
+                .nickname(request.nickname())
+                .password(passwordEncoder.encode(request.password()))
+                .build()
+        );
+
+        return jwtProvider.createToken(request.email());
+    }
+}
