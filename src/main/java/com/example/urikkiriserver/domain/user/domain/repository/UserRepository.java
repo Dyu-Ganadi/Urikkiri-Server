0ca65c35ebd5883923801c533af1b9c328e54e66
민수아 <Minsua0503@gmail.com>
2025-12-19 11:51:30 +0900

    feat: UserRepository

---
+++ src/main/java/com/example/urikkiriserver/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.urikkiriserver.domain.user.domain.repository;
+
+import com.example.urikkiriserver.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByNickname(String nickname);
+}
