7a1762ca057748b3f9ca474ff2b66cae726f8b40
sunnyday0206 <sunme8412@gmail.com>
2026-01-07 13:58:12 +0900

    feat: room entity and repository 추가

---
+++ src/main/java/com/example/urikkiriserver/domain/play/domain/Room.java
@@ -0,0 +1,25 @@
+package com.example.urikkiriserver.domain.play.domain;
+
+import com.example.urikkiriserver.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity(name = "tbl_room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(6)")
+    private String code;
+
+    @OneToOne()
+    @JoinColumn(name = "id", nullable = false)
+    private User manager;
+
+}
