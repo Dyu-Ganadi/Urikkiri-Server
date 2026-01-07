3c7351b2f21980a8bb4c8ab773965416c52f1678
sunnyday0206 <sunme8412@gmail.com>
2026-01-07 14:29:46 +0900

    feat: 어노테이션 정리

--- src/main/java/com/example/urikkiriserver/domain/play/domain/Room.java
+++ src/main/java/com/example/urikkiriserver/domain/play/domain/Room.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Entity(name = "tbl_room")
+@Entity
 @Table(name = "tbl_room")
 public class Room {
 
