f8510497137bbaa75bf11ee2604a63f27b8e43f8
sunnyday0206 <sunme8412@gmail.com>
2026-01-07 13:54:24 +0900

    feat: card entity and repository 추가

---
+++ src/main/java/com/example/urikkiriserver/domain/card/domain/Card.java
@@ -0,0 +1,22 @@
+package com.example.urikkiriserver.domain.card.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity(name = "tbl_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String word;
+
+    @Column(nullable = false)
+    private String meaning;
+}
