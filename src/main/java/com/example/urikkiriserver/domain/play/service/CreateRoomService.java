1621a4ff9acd0199c5291bf66a22f5cdfd21d9f0
민수아 <Minsua0503@gmail.com>
2026-01-09 16:13:51 +0900

    feat: CreateRoomService

---
+++ src/main/java/com/example/urikkiriserver/domain/play/service/CreateRoomService.java
@@ -0,0 +1,58 @@
+package com.example.urikkiriserver.domain.play.service;
+
+import com.example.urikkiriserver.domain.play.domain.Participant;
+import com.example.urikkiriserver.domain.play.domain.Room;
+import com.example.urikkiriserver.domain.play.domain.repository.ParticipantRepository;
+import com.example.urikkiriserver.domain.play.domain.repository.RoomRepository;
+import com.example.urikkiriserver.domain.play.presentation.RoomResponse;
+import com.example.urikkiriserver.domain.user.domain.User;
+import com.example.urikkiriserver.domain.user.service.UserFacade;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CreateRoomService {
+
+    private final UserFacade userFacade;
+    private final RoomRepository roomRepository;
+    private final ParticipantRepository participantRepository;
+    private final Random random = new Random();
+
+    @Transactional
+    public RoomResponse execute() {
+        User user = userFacade.getCurrentUser();
+
+        String roomCode = generateRoomCode();
+
+        Room savedRoom = roomRepository.save(
+            Room.builder()
+                .code(roomCode)
+                .build()
+        );
+
+        participantRepository.save(Participant.builder()
+            .userId(user)
+            .roomId(savedRoom)
+            .bananaScore(0)
+            .isExaminer(true)
+            .build());
+
+        return RoomResponse.of(roomCode);
+    }
+
+    private String generateRoomCode() {
+        String roomCode;
+        do {
+            // 100000 ~ 999999 범위의 6자리 숫자 생성
+            roomCode = String.format("%06d", random.nextInt(900000) + 100000);
+        } while (roomRepository.existsByCode(roomCode));
+
+        return roomCode;
+    }
+}
