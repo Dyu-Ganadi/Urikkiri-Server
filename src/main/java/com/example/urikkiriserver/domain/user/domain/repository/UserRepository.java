717c31870c04dd3f8e450385046c600feb62c18f
sunnyday0206 <sunme8412@gmail.com>
2026-01-12 18:53:08 +0900

    refactor: import 변경

--- src/main/java/com/example/urikkiriserver/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/urikkiriserver/domain/user/domain/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.example.urikkiriserver.domain.user.domain.repository;
 
 import com.example.urikkiriserver.domain.user.domain.User;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
