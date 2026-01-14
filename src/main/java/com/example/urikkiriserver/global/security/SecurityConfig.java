724b83e26e527b4f6e12fc86ee4e108e8442a01d
민수아 <Minsua0503@gmail.com>
2026-01-14 21:45:40 +0900

    refactor: 안 쓰는 의존성 제거

--- src/main/java/com/example/urikkiriserver/global/security/SecurityConfig.java
+++ src/main/java/com/example/urikkiriserver/global/security/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final ObjectMapper objectMapper;
-    private final StringRedisTemplate redisTemplate;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
