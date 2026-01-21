19504dd3ea9be711966fee388356b2676457f671
민수아 <Minsua0503@gmail.com>
2026-01-21 14:43:16 +0900

    refactor: 카드 제출 시 유저 닉네임 제거

--- src/main/java/com/example/urikkiriserver/global/websocket/dto/SubmittedCardInfo.java
+++ src/main/java/com/example/urikkiriserver/global/websocket/dto/SubmittedCardInfo.java
@@ -5,7 +5,6 @@ import com.example.urikkiriserver.domain.play.domain.Participant;
 
 public record SubmittedCardInfo(
         Long participantId,
-        String nickname,
         Long cardId,
         String word,
         String meaning
@@ -13,7 +12,6 @@ public record SubmittedCardInfo(
     public static SubmittedCardInfo of(Participant participant, Card card) {
         return new SubmittedCardInfo(
                 participant.getId(),
-                participant.getUserId().getNickname(),
                 card.getId(),
                 card.getWord(),
                 card.getMeaning()
